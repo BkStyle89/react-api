ec444fc84efe8a92e6a76b642599542063d91c49
BkStyle89 <kajzer@hotmail.it>
2026-03-17 12:32:47 +0100

    male and female actors in page milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react'
 
 function App() {
-  const api_url ="https://lanciweb.github.io/demo/api/actresses/" 
-  const api_link ="https://lanciweb.github.io/demo/api/actors/"
+  const api_url ="https://lanciweb.github.io/demo/api/actors/" 
+  const api_link ="https://lanciweb.github.io/demo/api/actresses/"
   const [actors, setActors] =useState([])
+  const [actresses,setActresses]=useState([])
 
 
-function getActors(){
+  function getActors(){
   fetch(api_url)
   .then(res=>res.json())
   .then(data=>{
@@ -15,18 +16,51 @@ function getActors(){
 }
 useEffect(getActors,[])
 
+ function getActesses(){
+  fetch(api_link)
+  .then(res=>res.json())
+  .then(data=>{
+    setActresses(data)
+  })
+}
+useEffect(getActesses,[]) 
+
   return (
     <>
       <div className='container'>
         <div className='row'>
+          <div>
                <ul>
-                {actors.map(actor => (
-                  <li key={actor.id}>
-                  <img src={actor.image} alt="" /><br />
-                  {actor.name}
-                  </li>
+                {actresses.map(actress => (
+                  <li className='col-2' key={actress.id}>
+                <div class="card" >
+                  <img src={actress.image} alt="" />
+                  <div class="card-body">
+                    <h5 class="card-title">{actress.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">{actress.nationality}</h6>
+                    <p class="card-text">{actress.biography}</p>
+                    
+                  </div>
+                </div>
+                </li>
                 ))}
                 </ul>
+                   <ul>
+                {actors.map(actor => (
+                  <li className='col-2' key={actor.id}>
+                <div class="card" >
+                  <img src={actor.image} alt="" />
+                  <div class="card-body">
+                    <h5 class="card-title">{actor.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">{actor.nationality}</h6>
+                    <p class="card-text">{actor.biography}</p>
+                    
+                  </div>
+                </div>
+                </li>
+                ))}
+                </ul>
+                </div>
           </div>
         </div>
     </>
