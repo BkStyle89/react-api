1e76cb43e87d96b9db615253386a0fc79c278a1d
BkStyle89 <kajzer@hotmail.it>
2026-03-17 13:00:57 +0100

    milestone 2 preparing the cards

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -32,30 +32,34 @@ useEffect(getActesses,[])
           <div>
                <ul>
                 {actresses.map(actress => (
-                  <li className='col-2' key={actress.id}>
-                    <div class="card" >
-                      <img src={actress.image} alt="" />
-                      <div class="card-body">
-                        <h5 class="card-title">{actress.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted ">{actress.nationality}</h6>
-                        <p class="card-text">{actress.biography}</p>
+                  <div className="col-lg-2 col-md-4 col-6">
+                    <li className='col-2' key={actress.id}>
+                      <div className="card" >
+                        <img src={actress.image} alt="" />
+                        <div class="card-body">
+                          <h5 class="card-title">{actress.name}</h5>
+                          <h6 class="card-subtitle mb-2 text-muted ">{actress.nationality}</h6>
+                          <p class="card-text">{actress.biography}</p>
+                        </div>
                       </div>
-                    </div>
-                  </li>
+                    </li>
+                  </div>
                 ))}
               </ul>
               <ul>
                 {actors.map(actor => (
-                  <li className='col-2' key={actor.id}>
-                    <div class="card" >
-                      <img src={actor.image} alt="" />
-                      <div class="card-body">
-                        <h5 class="card-title">{actor.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted ">{actor.nationality}</h6>
-                        <p class="card-text">{actor.biography}</p>
+                  <div className="col-lg-2 col-md-4 col-6">
+                    <li className='col-2' key={actor.id}>
+                      <div class="card" >
+                        <img src={actor.image} alt="" />
+                        <div class="card-body">
+                          <h5 class="card-title">{actor.name}</h5>
+                          <h6 class="card-subtitle mb-2 text-muted ">{actor.nationality}</h6>
+                          <p class="card-text">{actor.biography}</p>
+                        </div>
                       </div>
-                    </div>
-                  </li>
+                    </li>
+                  </div>
                 ))}
               </ul>
           </div>
