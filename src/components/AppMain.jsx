9599317beaa9137487e12c3b03d7102b45259fad
BkStyle89 <kajzer@hotmail.it>
2026-03-17 16:26:28 +0100

    milestone 2 all info

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -29,37 +29,39 @@ useEffect(getActesses,[])
   return (
     <div className='container'>
         <div className='row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 gx-5 gy-5'>
-                {actresses.map(actress => (
-                  <div  key={actress.id}>
-                    <div className="col">
-                      <div className="card" >
-                        <img src={actress.image} alt="" />
-                        <div class="card-body">
-                          <h5 class="card-title">{actress.name}</h5>
-                          <h6 class="card-subtitle mb-2 text-muted ">{actress.nationality}</h6>
-                          <p class="card-text">{actress.biography}</p>
-                          <p>{actress.birth_year} </p>
-                        </div>
+              {actresses.map(actress => (
+                <div  key={actress.id}>
+                  <div className="col">
+                    <div className="card" >
+                      <img src={actress.image} alt="" />
+                      <div class="card-body">
+                        <h5 class="card-title">{actress.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted ">{actress.nationality}</h6>
+                        <p class="card-text">{actress.biography}</p>
+                        <p>{actress.awards}</p>
+                        <p>{actress.birth_year} </p>
                       </div>
                     </div>
                   </div>
-                ))}
-                {actors.map(actor => (
-                  <div key={actor.id}>
-                    <div className="col">
-                      <div class="card" >
-                        <img src={actor.image} alt="" />
-                        <div class="card-body">
-                          <h5 class="card-title">{actor.name}</h5>
-                          <h6 class="card-subtitle mb-2 text-muted ">{actor.nationality}</h6>
-                          <p class="card-text">{actor.biography}</p>
-                          <p>{actor.birth_year} </p>
-                        </div>
+                </div>
+              ))}
+              {actors.map(actor => (
+                <div key={actor.id}>
+                  <div className="col">
+                    <div class="card" >
+                      <img src={actor.image} alt="" />
+                      <div class="card-body">
+                        <h5 class="card-title">{actor.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted ">{actor.nationality}</h6>
+                        <p class="card-text">{actor.biography}</p>
+                        <p>{actor.awards}</p>
+                        <p>{actor.birth_year} </p>
                       </div>
                     </div>
                   </div>
-                ))}
-          </div>
+                </div>
+              ))}
         </div>
+    </div>
   )
 }
